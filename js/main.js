bcc84e4150ed2792a760684dc6348ace5c6080d1
Alec Raeside <alec@AlMac.local>
2012-06-21 21:02:00 +1000

    get manual wall collision detection working

--- js/main.js
+++ js/main.js
@@ -6,6 +6,12 @@
     var radius = 400;
     var theta = 0;
 
+    var pong_box = {
+            width:400,
+            height:400,
+            depth:800
+    }
+
     window.onload=function() { init(); };
 
     function init() {
@@ -33,9 +39,12 @@
         lightmid.position = {
             x : 0,
             y : 0,
-            z : 300,
+            z : 500,
         }
-        scene.add( lightmid );  
+        //scene.add( lightmid );
+
+        var amb_light = new THREE.AmbientLight(0xffffff);
+        scene.add(amb_light)  
          
         light = new THREE.DirectionalLight( 0xffffff,1);
         light.postion = {
@@ -45,11 +54,11 @@
         }
         light.position.normalize()
 
-       // scene.add( light );   
+        //scene.add( light );   
     }
     function camera() {
         camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
-        camera.position.set( 10, 110, 700 );
+        camera.position.set( 10, 110, (pong_box.depth/2)+200 );
         scene.add( camera );
         camera.lookAt( scene.position );
     }
@@ -57,11 +66,11 @@
     function action() {
         //To use enter the axis length
         //debugaxis(1000);
-        var left_side = createSide();
-        var top_side = createSide();
-        var right_side = createSide();
-        var bottom_side = createSide();
-        solid_objects=[left_side,top_side,right_side,bottom_side]
+
+        left_side = createSide();
+        top_side = createSide();
+        right_side = createSide();
+        bottom_side = createSide();
 
         left_side.position.x=-200;
         left_side.rotation.z=Math.PI/2;
@@ -71,9 +80,21 @@
         right_side.rotation.z=Math.PI/2;
         bottom_side.position.y=-200;
 
+        player = new THREE.Mesh( 
+                             new THREE.CubeGeometry(40,40,1),
+                            new THREE.MeshLambertMaterial({
+                                ambient: 0x0000ff,
+                                color: 0x0000ff,
+                                shading: THREE.SmoothShading 
+                            })
+                    );
+        player.material.opacity=0.3;
+        player.position.z=pong_box.depth/2
+        scene.add(player)
+
         sphere = new THREE.Mesh(new THREE.SphereGeometry(20, 50, 50), 
              new THREE.MeshLambertMaterial({
-                                //ambient: 0xffffff,
+                                ambient: 0xffffff,
                                 color: 0xffffff,
                                 specular:0xffffff, 
                                 shininess: 10, 
@@ -83,22 +104,26 @@
         sphere.position.x=110;
         sphere.position.y=100;
 
+        sphere.vx=Math.random()-0.5;
+        sphere.vy=Math.random()-0.5;
+        sphere.vz=Math.random()-0.5;
 
         scene.add(sphere);
-       
+  
     }
 
     function createSide() {
-        var side_geo = new THREE.CubeGeometry(400,22,800)
+        var side_geo = new THREE.CubeGeometry(pong_box.width,2,pong_box.depth)
         var side_params = {
-                            ambient: 0xffffff,
-                            color: 0xffffff, 
+                            ambient: Math.random()*0xffffff,
+                            color: Math.random()*0xffffff,
+                            specular:Math.random()*0xffffff, 
                             shading: THREE.SmoothShading 
                         }
 
         object = new THREE.Mesh( 
                             side_geo,
-                            new THREE.MeshPhongMaterial(side_params)
+                            new THREE.MeshLambertMaterial(side_params)
                     );
          scene.add(object)
         return object;
@@ -106,10 +131,29 @@
 
     function animate() {
         requestAnimationFrame( animate );
+       //setTimeout(animate,2000);
         stats.begin();
-       
+        checkCollisions()
+        moveBall();
         render();
         stats.end()
+    }
+    function checkCollisions() {
+        if (Math.abs(sphere.position.x) >= right_side.position.x) {
+            sphere.vx*=-1;
+        }
+        if (Math.abs(sphere.position.y) >= top_side.position.y) {
+            sphere.vy*=-1;
+        }
+    }
+
+    speed_multiplier = 10;
+    function moveBall() {
+
+        sphere.position.x+=sphere.vx*speed_multiplier;
+        sphere.position.y+=sphere.vy*speed_multiplier;
+        sphere.position.z+=sphere.vz*speed_multiplier;
+        //console.log(sphere.position.x,sphere.position.y,sphere.position.z)
     }
 
     radius=50;
@@ -124,3 +168,4 @@
         renderer.render( scene, camera );
 
     }
+

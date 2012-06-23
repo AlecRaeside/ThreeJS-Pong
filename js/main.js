c277e888ff3443d9a3449c0e7317684d7a1fdf5d
Alec Raeside <alec@AlMac.local>
2012-06-23 23:42:52 +1000

    get sound effects working

--- js/main.js
+++ js/main.js
@@ -20,11 +20,18 @@ var pong_box = {
 }
 var sphere_radius=20;
 
+var ball_sound0 = new buzz.sound("ballbounce.wav");
+var ball_sound1 = new buzz.sound("ballbounce.wav");
+var ball_sound2 = new buzz.sound("ballbounce2.mp3");
+ball_sound2.setVolume( 20 )
+var player_ball_sound = new buzz.sound("ballbounce2.mp3");
+
 window.onload=function() { init(); };
 
 function init() {
     stats = new Stats();
     document.body.appendChild( stats.domElement );
+
     scene = new THREE.Scene();
 
     renderer = new THREE.WebGLRenderer();
@@ -36,7 +43,9 @@ function init() {
     camera();
     action();
     
+
     document.body.appendChild( renderer.domElement );
+    renderer.domElement.style.cursor = "none"
 
     animate();
 }
@@ -50,7 +59,7 @@ function lights() {
         y : 0,
         z : (pong_box.depth/2)+50,
     }
-    scene.add( lightbehind );
+    //scene.add( lightbehind );
 
 
 
@@ -61,16 +70,16 @@ function lights() {
         y : 0,
         z : 0,
     }
-   // scene.add( sphere_light );
+    scene.add( sphere_light );
 
     var amb_light = new THREE.AmbientLight(0xffffff);
     //scene.add(amb_light)  
      
     light = new THREE.DirectionalLight( 0xffffff,0.7,400);
-    light.position = new THREE.Vector3(1,1,1)
+    light.position = new THREE.Vector3(2,1,1)
     scene.add( light );  
     light = new THREE.DirectionalLight( 0xffffff,0.7,400);
-    light.position = new THREE.Vector3(-1,1,1)
+    light.position = new THREE.Vector3(-2,1,1)
     scene.add( light );
     light = new THREE.DirectionalLight( 0xffffff,0.5,400);
     light.position = new THREE.Vector3(1,-1,1)
@@ -113,9 +122,23 @@ function action() {
                 );
     player.material.opacity=0.2;
     player.material.transparent=true;
- 
+
     player.position.z=pong_box.depth/2
     scene.add(player)
+
+    opponent = new THREE.Mesh( 
+                     new THREE.CubeGeometry(player_size.width,player_size.height,3),
+                    new THREE.MeshLambertMaterial({
+                        color: 0xff5555,
+                        shading: THREE.SmoothShading 
+                    })
+            );
+    opponent.material.opacity=0.8;
+    opponent.material.transparent=true;
+    
+    opponent.position.z = ((pong_box.depth/2) * -1)-3
+    scene.add(opponent)
+
 
     sphere = new THREE.Mesh(new THREE.SphereGeometry(sphere_radius,10,10), 
          new THREE.MeshLambertMaterial({
@@ -174,7 +197,9 @@ function createSide() {
 }
 
 function animate() {
-    requestAnimationFrame( animate );
+    if (sphere.position.z < (pong_box.depth/2)+100) {
+        requestAnimationFrame( animate );
+    }
     //setTimeout(animate,2000);
     stats.begin();
 
@@ -187,26 +212,35 @@ function animate() {
 }
 function checkCollisions() {
     if (Math.abs(sphere.position.x) + sphere_radius >= right_side.position.x) {
+       
+         ball_sound0.setVolume( getWallBounceVolume(sphere.position.z) )
+          ball_sound0.play();
         sphere.vx*=-1;
     }
     if (Math.abs(sphere.position.y) + sphere_radius >= top_side.position.y) {
         sphere.vy*=-1;
+        ball_sound1.setVolume( getWallBounceVolume(sphere.position.z) )
+         ball_sound1.play();
     }
     if (sphere.position.z < (pong_box.depth/2)*-1) {
         sphere.vz*=-1;
+        ball_sound2.play();
     }
-    //check for player collisions
+    //check for player collisions if ball is near player's movement plane
     if (sphere.vz > 0 && numCloseTo(sphere.position.z, pong_box.depth/2, speed_multiplier)) {
-        //console.log(Math.abs(sphere.position.z-(pong_box.depth/2)))
-        var x_hit = numCloseTo(sphere.position.x, player.position.x,player_size.width);
-        var y_hit = numCloseTo(sphere.position.y, player.position.y,player_size.height);
-            //console.log(x_hit,sphere.position.x,player.position.x)
-            //console.log(y_hit,sphere.position.y,player.position.y)
+       
+        var x_hit = numCloseTo(sphere.position.x, player.position.x,player_size.width + sphere_radius);
+        var y_hit = numCloseTo(sphere.position.y, player.position.y,player_size.height + sphere_radius);
+           
         if (x_hit && y_hit) {
             sphere.vz*=-1;
+            player_ball_sound.play();
         }
         
     }
+}
+function getWallBounceVolume(sphere_z) {
+     return ( ( (sphere_z + (pong_box.depth/2) ) / pong_box.depth) * 45) + 15;
 }
 
 
@@ -216,9 +250,15 @@ function moveBall() {
     sphere.position.y+=sphere.vy;
     sphere.position.z+=sphere.vz;
 
+
+
+    opponent.position.x = sphere.position.x;
+    opponent.position.y = sphere.position.y;
+
     sphere_light.position.x = sphere.position.x;
     sphere_light.position.y = sphere.position.y;
-    sphere_light.position.z = sphere.position.z;
+
+    sphere_light.position.z = sphere.position.z + sphere_radius+2;
     //console.log(sphere.position.x,sphere.position.y,sphere.position.z)
 }
 
